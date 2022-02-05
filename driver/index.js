272575d677447457cc29927a3447a84b7d8305d7
erik savage <erik.k.savage@gmail.com>
2022-02-05 13:15:29 -0800

    updates queueUrl

--- driver/index.js
+++ driver/index.js
@@ -3,9 +3,9 @@
 const { Consumer } = require('sqs-consumer');
 
 const app = Consumer.create({
-  queueUrl: 'https://sqs.us-west-2.amazonaws.com/130781547038/pickup',
+  queueUrl: 'https://sqs.us-west-2.amazonaws.com/130781547038/packages.fifo',
   handleMessage: (message) => {
-    console.log(message);
+    console.log(JSON.parse(message.Body));
   },
 });
 
