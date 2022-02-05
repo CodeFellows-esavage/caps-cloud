e9a28c0cd255fa3379e4e064d7c7ff2fbe46ccab
erik savage <erik.k.savage@gmail.com>
2022-02-05 13:09:18 -0800

    adds MessageGroupId, stringifies shipment obj

--- vendor/index.js
+++ vendor/index.js
@@ -9,15 +9,18 @@ AWS.config.update({ region: 'us-west-2' });
 const sns = new AWS.SNS();
 //the topic this message is published to on AWS
 const topic = 'arn:aws:sns:us-west-2:130781547038:pickup.fifo';
+const vendorArn = 'arn:aws:sqs:us-west-2:130781547038:vendor';
 
 const shipment = {
   orderId: nanoid(),
   customer: `${randName.first()} ${randName.last()}`,
-  vendorId: 'arn:aws:sqs:us-west-2:130781547038:vendor',
+  vendorId: vendorArn,
 };
 
+//using vendor arn for MessageGroupId so each vendors groupId is unique and processed in FIFO;
 const payload = {
-  Message: shipment,
+  Message: JSON.stringify(shipment),
+  MessageGroupId: vendorArn,
   TopicArn: topic,
 };
 
