6c45394449dcb14ec9d53ce10936f84d3b0a437b
erik savage <erik.k.savage@gmail.com>
2022-02-05 11:30:24 -0800

    adds shipment object for Message payload

--- vendor/index.js
+++ vendor/index.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { nanoid } = require('nanoid');
+const randName = require('random-name');
+
 AWS.config.update({ region: 'us-west-2' });
 
 const sns = new AWS.SNS();
+//the topic this message is published to on AWS
 const topic = 'arn:aws:sns:us-west-2:130781547038:pickup';
 
+const shipment = {
+  orderId: nanoid(),
+  customer: `${randName.first()} ${randName.last()}`,
+  vendorId: 'arn:aws:sqs:us-west-2:130781547038:vendor',
+};
+
 const payload = {
-  Message: 'PICK UP READY!',
+  Message: shipment,
   TopicArn: topic,
 };
 
 sns.publish(payload).promise()
   .then(data => console.log(data))
   .catch(err => console.log(err));
+
