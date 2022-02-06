8b0b503b5f31f088a7b8f85542644c931217df9d
erik savage <erik.k.savage@gmail.com>
2022-02-05 16:04:43 -0800

    adds interval to publish a new shipment, adds ability to consume deliveries

--- vendor/index.js
+++ vendor/index.js
@@ -1,30 +1,45 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { Consumer } = require('sqs-consumer');
 const { nanoid } = require('nanoid');
 const randName = require('random-name');
 
 AWS.config.update({ region: 'us-west-2' });
 
-const sns = new AWS.SNS();
 //the topic this message is published to on AWS
 const topic = 'arn:aws:sns:us-west-2:130781547038:pickup.fifo';
 const vendorArn = 'arn:aws:sqs:us-west-2:130781547038:vendor';
 
-const shipment = {
-  orderId: nanoid(),
-  customer: `${randName.first()} ${randName.last()}`,
-  vendorId: vendorArn,
-};
+setInterval(() => {
+  const sns = new AWS.SNS();
 
-//using vendor arn for MessageGroupId so each vendors groupId is unique and processed in FIFO;
-const payload = {
-  Message: JSON.stringify(shipment),
-  MessageGroupId: vendorArn,
-  TopicArn: topic,
-};
+  const shipment = {
+    orderId: nanoid(),
+    customer: `${randName.first()} ${randName.last()}`,
+    vendorId: vendorArn,
+  };
 
-sns.publish(payload).promise()
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+  //using vendor arn for MessageGroupId so each vendors groupId is unique and processed in FIFO;
+  const payload = {
+    Message: JSON.stringify(shipment),
+    MessageGroupId: vendorArn,
+    TopicArn: topic,
+  };
 
+  sns.publish(payload).promise()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}, 10000);
+
+
+const app = Consumer.create({
+  queueUrl: 'https://sqs.us-west-2.amazonaws.com/130781547038/vendor',
+  handleMessage: (message) => {
+    const messageBody = JSON.parse(message.Body);
+    const deliveredMessage = messageBody.Message;
+    console.log(deliveredMessage);
+  },
+});
+
+app.start();
